0b76a1c826550df0cd2b1ef05add50943484fe38
Ben B <lerminator3@gmail.com>
2022-02-07 12:50:06 -0500

    use constants

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -2,12 +2,14 @@ package frc.robot.subsystems;
 
 import org.frc5587.lib.subsystems.SimpleMotorBase;
 
+import frc.robot.Constants.IntakeConstants;
+
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 public class Intake extends SimpleMotorBase {
-    private static CANSparkMax motor = new CANSparkMax(25, MotorType.kBrushless);
+    private static CANSparkMax motor = new CANSparkMax(IntakeConstants.MOTOR, MotorType.kBrushless);
 
     public Intake() {
         super(motor);
@@ -17,7 +19,7 @@ public class Intake extends SimpleMotorBase {
     public void configureMotors() {
         motor.restoreFactoryDefaults();
 
-        motor.setInverted(false);
+        motor.setInverted(IntakeConstants.INVERTED);
 
         motor.setIdleMode(IdleMode.kCoast);
     }
