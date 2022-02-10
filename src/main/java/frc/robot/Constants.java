aabc297bddfd2bf74d420d7fc2bf6c8e35a7ac2b
Andrew Lawlor <lawlorcube06@gmail.com>
2022-02-10 15:49:52 -0500

    removed unused constants import

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,10 +4,6 @@
 
 package frc.robot;
 
-import org.frc5587.lib.pid.PID;
-
-import edu.wpi.first.math.trajectory.TrapezoidProfile;
-
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
  * constants. This class should not be used for any other purpose. All constants should be declared
