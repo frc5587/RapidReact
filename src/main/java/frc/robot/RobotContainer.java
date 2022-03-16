83b6c2dae27f2aad8efe957994e86c7016dc8fd7
Ben B <lerminator3@gmail.com>
2022-03-16 18:08:41 -0400

    change configureButtonBindings doc

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -72,12 +72,8 @@ public class RobotContainer {
     }
 
     /**
-     * Use this method to define your button->command mappings. Buttons can be
-     * created by
-     * instantiating a {@link GenericHID} or one of its subclasses ({@link
-     * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing
-     * it to a {@link
-     * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
+     * Use this method to define your button->command mappings. Buttons are created
+     * within {@link DeadbandXboxController}.
      */
     private void configureButtonBindings() {
         Trigger limelightTrigger = new Trigger(limelight::hasTarget);
